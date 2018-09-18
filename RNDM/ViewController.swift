83b86efaf03568ea5eff8015716419e48afce227
Ryan Gjoraas <ryan.gjoraas@gmail.com>
2018-09-17 21:57:06 -0400

    Initial Commit

---
+++ RNDM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RNDM
+//
+//  Created by Ryan Gjoraas on 9/17/18.
+//  Copyright © 2018 Developed by Gjoraas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
